95d23f5337967472ac94eff34ee19c420d5c589f
santiago859 <113222164+santiago859@users.noreply.github.com>
2022-09-11 09:36:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si uwu')
+    alert('Sabia que ibas a decir que si amor')
 });
 
 const noBtn = document.querySelector('#noBtn');
